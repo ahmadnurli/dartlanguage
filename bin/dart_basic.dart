252fa553e62bc4c76e92631fa98b1a6e4ab39b05
ahmad <ahmad.nurli@moratelindo.co.id>
2022-06-03 10:54:35 +0700

    variable declaration and initialization

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,5 +1,10 @@
 void main(List<String> args) {
   // prints statement Hello world and another else statement
-  print('Hello world');
+  /*print('Hello world');
   print('I like learning');
+  */
+
+  // variable declaration and initialization
+  String name = 'Ahmad';
+  print(name);
 }
