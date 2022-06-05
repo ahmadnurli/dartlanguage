7d7513273cd7951a1731c6510e4e534ce462861b
ahmad <ahmad.nurli@moratelindo.co.id>
2022-06-05 08:35:39 +0700

    [Exercise] Printing variables

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,26 +1,10 @@
 void main(List<String> args) {
-  String name = 'Ahmad Nurli';
-
-  ///mendeklarasikan dgn nama variabel age bertipe int dan inisialisasi nilai integer 36
-  ///int: up to 64-bit precision
-  ///Dart VM: from -2^63 to 2^63-1
-  ///Transpiled to JavaScript: from -2^53 to 2^53-1
-  int age = 36;
-
-  ///floating point number
-  ///double: 63-bit floating point numbers
-  ///double precision
+  String firstName = 'Ahmad';
+  String lastName = 'Nurli';
+  int age = 40;
   double height = 1.84;
-
-  ///bool adalah tipe data yang bisa digunakan 2 nilai kemungkinan: 'true' dan 'false'
-  bool likesDart = true;
-
-  print(name);
-
-  ///lakukan statement print variabel age
+  print(firstName);
+  print(lastName);
   print(age);
-
   print(height);
-
-  print(likesDart);
 }
