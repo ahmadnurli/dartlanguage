63f8c79dcd9de76719641dbe2a6e0c32f6f49bcb
ahmad <ahmad.nurli@moratelindo.co.id>
2022-06-05 07:15:11 +0700

    Variable declaration and initialization

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,7 +1,25 @@
 void main(List<String> args) {
-  ///memanggil print statement
-  ///method print menampilkan string Hello World atau object pada konsol
-  print('Hello World');
-  ///semua kode di eksekusi secara baris perbaris dari atas ke bawah
-  print('i like learning');
+  ///mendefinisikan sebuah variabel bernama [name], dan variabel ini bisa kita gunakan kapan saja
+  ///menjadi referensi untuk menggunakan variabel ini
+  ///jadi apa yang akan terjadi ketika kita mendifinisikan variabel?
+  ///memory akan mengalokasikan ke dalam sistem kita
+  ///
+  ///untuk mengakses nilai yang sudah tersimpan di memory,
+  ///kita bisa mengggunakan variabel name.
+  String name = 'Ahmad Nurli';
+
+  ///sehingga ketika kita print variabel name,
+  ///program kita akan menerima sebuah nilai dari variabel name
+  ///dan menampilkan nilai tersebut di konsol
+  print(name);
+
+  ///String name => deklarasi variabel(proses mendefinisikan variabel dan menamakan variabel tsb dengan name agar bisa digunakan)
+  ///= => assignment operator
+  ///'Ahmad Nurli' => inisialisasi variabel(proses inisialisasi nilai ke variabel)
+  ///; => terminator
+  ///
+  ///aturan pembuatan variabel:
+  ///-kombinasi dengan huruf kecil, huruf besar, angka, karakter underscore(myName1_2)
+  ///-tidak boleh di mulai dengan digit (8delapan)
+  ///-camelCase (myFirstName)
 }
