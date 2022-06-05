378d6c332e136f73c48a08e7ee76afac975c05f6
ahmad <ahmad.nurli@moratelindo.co.id>
2022-06-05 14:54:18 +0700

    Multi-line strings

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,60 +1,44 @@
 void main(List<String> args) {
-  ///pada latihan sebelumnya ketika mendefinisikan string kita bisa menggunakan
-  ///single atau double quotes
-  ///untuk mengilustrasikan kenapa menggunakn 2 cara tsb diatas sbg berikut:
+  ///kita punya tiga string terpisah seperti dibawah ini
+  print('This is a short sentence');
+  print('This is a short sentence,  I dare say');
+  print(
+      'This is an evan longer sentence, which will not fit inside a single line');
+
+  ///dan biasanya digunakan untuk merepressentasikan beberapa baris dalam satu string
+  ///dengan satu cara melakukan semua 3 string diatas dalam satu print statement seperti ini:
   ///
-  ///buat kalimat dengan single quote
-  ///statemen ini akan terjadi error: Expected to fing ','
-  ///karena memilih menggunakan single quotes pada string pembatas(I'm)
-  ///Dart berasumsi kutip pada I'm adalah akhir dari string,
-  ///dan kalimat(string) selanjutnya menjadi invalid syntax
-  // print('Today I'm feeling great');
+  ///ini kode yang valid, tp jika di jalankan hasil yang kita lihat
+  ///strings tsb telah dilipat/gabung/satukan dalam satu baris
+  ///
+  ///sehingga kita ingin memiliki satu baris baru di akhir pada setiap strings
+  print('This is a short sentence.'
+      'This is a short sentence,  I dare say,'
+      'This is an evan longer sentence, which will not fit inside a single line');
 
-  ///solusinya gunakan double quotes
-  ///sehingga Dart tidak bingung
-  print("Today I'm feeling great");
+  ///satu cara untuk melakukannya menggunakan satu baris baru dengan karakter spesial
+  ///dengan bisa mendefiniskannya dengan sintak back-slash n '\n'
+  print('This is a short sentence.\n'
+      'This is a short sentence,  I dare say.\n'
+      'This is an evan longer sentence, which will not fit inside a single line.\n');
 
-  ///solusi cara lain menggunakan backslash sebelum single quotes didalam string
-  ///cara ini memberitahu Dart bahwa karakter \ bagian dari pada string
-  ///dan ini sebut dengan String Escaping
-  ///yaitu dgn menambahkan sebuah karakter escaping di depan karakter spesial seperti single quote
-  print('Today I\'m feeling great');
+  ///tetapi jika kita mempunyai banyak sekali strings,
+  ///akan sedikit membosankan untuk menambahkan baris karakter baru setiap akhir baris
+  ///
+  ///kita bisa melakukan multi line syntax, yg dapat dilakukan adalah
+  ///mendefinisikan strings dengan tiga double quotes di awal dan di akhir seperti ini:
+  print('''
+  This is a short sentence.
+  This is a short sentence,  I dare say.
+  This is an evan longer sentence, which will not fit inside a single line.
+  ''');
 
-  ///ketika black-slash menyatakan dirinya karakter spesial
-  ///jika kita ingin menggunakan black-slash didalam string,
-  ///string tsb juga harus memiliki black-slash
-  ///hal ini konsol akan menampilkan single black-slash
-  print('\\');
+  ///kita juga bisa melakukan atau menyimpan ke dalam sebuah variabel
+  String s = """
+  This is a short sentence.
+  This is a short sentence,  I dare say.
+  This is an evan longer sentence, which will not fit inside a single line.
+""";
 
-  ///juga ketika akan menampilkan simbol dollar, jika tidak dimaksudkan untuk string interpolasi
-  ///sehingga String escaping sangat berguna ketika bertujuan untuk menghasilkan sepsial karakter
-  print('\$');
-
-  ///ada beberapa string yang mengandung banyak spesial karakter
-  ///contoh: tipical path yang merepresentasikan penggunaan black-slash direktori sistem on windows
-  ///ini akan terlihat membosankan hehe
-  print('C:\\Windows\\system32');
-
-  ///Dart juga mendukung string literal khusus yang di sebut dgn raw string
-  ///tambahkan huruf kecil r sebelum pembatas, sehingga tidak lagi memerlukan Escaping special characters
-  ///sintak ini tidak dipakai secara umum, tp kita harus tau perihal ke eksissannya
-  ///gunakan cara ini ketika dibutuhkan
-  print(r'C:\\Windows\system32');
-
-  ///jika kita lebih suka menggunakan single atau double quotes
-  ///sebgian besar Dart menggunakan single quotes
-  ///dan lebih konsisten
-
-  /*little quiz*/
-  ///bisa kah anda tebak output mana dari kode dibawah ini yang tidak berjalan?
-  int a = 10;
-
-  ///hanya menggunakan string interpolation untuk menampilkan nilai
-  print('$a');
-
-  ///menggunakan excape character untuk tanda dollar, dan mencetak huruf a setelahnya
-  print('\$a');
-
-  ///mencetak tanda dollar yang lain(kedua) dan menggunakan interpolation nilai dari variable a
-  print('\$$a');
+  print(s);
 }
