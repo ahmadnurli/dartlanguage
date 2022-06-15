92eab60b9abddbfa6e1e77b8877932bbae67a52c
ahmad <ahmad.nurli@moratelindo.co.id>
2022-06-15 08:34:49 +0700

    Expressions & statements

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,49 +1,31 @@
 void main(List<String> args) {
-  ///ada satu fitur dalam Bhs Dart yang belum kita bahas
-  ///dan karena fitur ini sama sekali tidak mempengaruhi prilaku program kita
-  ///namun ini sangat berharga dan berguna bagi orang orang yang perlu membaca kode kita
-  ///fitur ini di nama comments
-  ///comments adalah sebauh blok koode yang akan diabaikan oleh compiler
-  ///comments normalnya digunakan sebagai document pada kode,
-  ///agar kode yang kita buat lebih mudah dimengerti
+  ///sampai degn saat ini kita menggunakan kobinasi antara
+  ///statement dan expressions pada kode program
   ///
-  ///jadi dalam leasson ini kita akan belajar ttg single dan multiline comments
-  ///(//, ///), (/* */) comments
-  ///dan bagaimana cara menggunakan comment tsb dalam memproduksi document
-  ///serta mengingatkan kita akan tugas-tugas yang perlu kita lakukan kedepannya
+  ///jadi apa yg dimaksud expressions?
+  ///setiap integer literal dibawah ini adalah expressions
+  ///dan satu baris berikut merupakan kombinasi expressions
+  ///yang diterapkan kedalam sebuah variable
+  ///(10 + 3) % 4
   ///
-  ///dan saya juga akan memebrikan beberapa panduan ttg kapan dan mengapa kita
-  ///harus menulis komentar
-  ///
-  ///kita mulai dengan single comments yang menggunakan 2 forward slash // diawal
-  //the entry point for our program
+  ///dan keseluruhan dari baris ini disebut statement
+  int x = (10 + 3) % 4;
 
-  //triple forward slash
-  ///Dart akan mengenerate sebuah decumentation kode program
+  //ini juga statment
+  print(x);
 
-  ///tipe comment lainnya menggunakan double slash
-  ///seperti TODOs, ini bukan bagian dari documentasi kode program
-  ///melainkan mengindikasikan kebutuhan sebuah kode program di dalam main method
-  ///yang mengingatkan kita akan sebuah tugas yang akan kita kerjakan
-  // TODO: Implement me.
+  ///kita tidak bisa menerapkan statment ke sebuah variable
+  ///karena statement print(x); tidak mempunyai nilai
+  // int y = print(x);
 
-  //ini yang di sebut multi lint comment
-  //yang bisa kita gunakan ketika kita menulis blok teks besar
+  //perbedaan expressions dan statement
   /**
-   * version 1.5 will include:
-   * - user authentication with Firebase
-   * - sorting results by date
-   * - generate reports
+   * expression menahan atau memegang nilai ketika saat runtime
+   * 
+   * statements berisi expressions tapi tidak menahan atau memegang nilai ketika saat runtime
    */
-
-  //================================
-  /**
-   * kapan kita harus menggnakan comment dan kenapa
-   * - tergantung apa yang akan kita tulis(apps/libraries/open source)?
-   * - My Opinion:
-   *  ;sebagai penjelasan kenapa kita menulis kode dgn cara spesifik, jika 
-   *    konteks program dmk kurang cukup jelas
-   *  ;jangan menulis komentar untuk menjelaskan apa yang akan dilakukan oleh kode pemrograman kita
-   *    karena seharusnya kode program tsb akan menjadi jelas dengan membaca kode tsb
-   *  ;*/
+  
+  ///ada beberapa bahasa pemrograman lain dimana statemens adalah expressions, 
+  ///tapi Dart tidak dibilang seperti demikian 
 }
+
