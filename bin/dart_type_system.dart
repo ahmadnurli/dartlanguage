458d2d2cb618c7c35753d41799235dc4f5c30a49
ahmad <ahmad.nurli@moratelindo.co.id>
2022-06-19 19:50:17 +0700

    The final keyword

--- bin/dart_type_system.dart
+++ bin/dart_type_system.dart
@@ -1,23 +1,61 @@
 void main(List<String> args) {
-  ///pada kali ini kita akan belajat type inference with var
-  ///Dart dapat menyimpulkan jenis variable yang kita deklarasikan
-  ///artinya disini kita tidak butuh mendeklarasikan variable secara explisite sprt kode dibawah
-  String name = 'ahmad';
-  int age = 22;
-  double height = 65.6;
+  ///Ada akalanya waktu di mana kita ingin mendeklarasikan dan inisialisasi sebuah variable
+  ///yang tidak akan mengubah nilai setelahnya, dgn kata lain kita ingin varible kita hanya bisa di baca saja
+  ///oleh karena itu kita bisa menggunakan 'final' keyward seperti dibawah ini:
+  ///dgn begitu kita memberitahu Dart,
+  ///mendeklarasikan dan menginisialisasi final variable untuk kita, dan jangan mengubahnya lagi
+  final name = 'ahmad';
+  final age = 22;
+  final height = 65.6;
 
-  ///sbg gantinya kita bisa menggunakan keyword 'var'
-  ///dart akan menyimpulkan(infer) bahwa variable memiliki tipe string,
-  ///karena diinisialisasi dgn string literal, begitu juga dgn int dan double
-  var nama = 'ahmad';
-  var umur = 22;
+  ///ketika kita coba untuk mengetik sprti dibawah ini:
+  ///dart akan memberitahu bahwa final variable age hanya bisa di set satu kali
+  // age = 21;
 
-  ///dan jika mau kita bisa menerapkan(assign) lagi variable umur dengan nilai berbeda
-  ///selama nilai tsb bernilai int
-  ///tapi kita tidak bisa menerapkan dgn tipe nilai yg berbeda
-  // umur = true;
-  ///karena tipe dari variable tsb disimpulkan ke int saat inisialisasi, 
-  ///oleh karena itu tidak bisa diubah ke tipe yg lain
-  umur = 23;
-  var tinggi = 65.6;
+  ///lalu kenapa kita melakukan hal diatas
+  ///jika menggunakan var, kita bisa menerapkan kembali dgn nilai berbeda
+  ///dgn pengertian tsb, penggunaan final menjadi lebih terbatas
+  ///
+  ///sebenarnya ini sesuatu yang baik, karena jika kita membatasi jumlah kemungkinan state
+  ///yang dapat dimasuki oleh program kita, kemungkinan kecilnya akan menimbulkan bug
+  ///
+  ///sebagai contoh program sederhana pada sebuah string yg akan diubah menjadi huruf besar
+  ///dan jika kita mendeklarasikannya dengan var,
+  ///tidak terjadi hal apapun dalam menerapkan method uppercase dibawah ini
+  var title = 'Dart course';
+  var titleUppercased = title.toUpperCase();
+  print(titleUppercased);
+
+  ///tetapi jika kita bertujuan menginisialisasi titel dgn nilai baru dan
+  ///mengkonversi menjadi huruf besar, seharusnya tidak kita lakukan demikian
+  var titel = 'Dart course';
+  titel = '24';
+
+  ///dan karena alasan ini, lebih baik mendeklarasikan variable tsb sbg final
+  ///sehingga dart akan memberitahu bahwa kita tidak merubah final variable
+  final titles = 'Dart course';
+  // titles = '25';
+
+  ///begitu juga setelah kita memperkirakan hasil keluaran dari variable tidak berubah,
+  ///maka kita harus mendeklarasikannya sbg final juga
+  final titlesUppercased = titles.toUpperCase();
+  print(titlesUppercased);
+
+  ///tentu sj ini hanya program yang simple,
+  ///dan mungkin kita tidak yakin bahwa ini ada masalah besar
+  ///tapi saat kita mempunya program yang lebih besar dgn banyak kode
+  ///maka menggunakan final akan benar2 sangat terbantu,
+  ///dan juga memberitahu kita variable mana sj yg ditetapkan hanya bisa untuk dibaca
+  ///
+  ///jadi, kesimpulan dari pelajaran ini adalah bahwa final berarti hanya bisa dibaca,
+  ///dan kita harus memilih/menetapkan final atau var bila memungkinkan
+  ///
+  ///btw, kita kita menggunakan final, kita masih bisa diizinkan untuk secara eksplisit
+  ///mendeklarasikan jenis variable kita
+  ///dalam artian kita bisa menulis kode kita sprt berikut:
+  ///bagaiamanapun jg ini tidak diperlukan, karena Dart akan menyimpulkan(infer)
+  ///tipe variable untuk kita. jadi kita tidak membutuhkannya
+  final String judul = 'Dart course';
+
+  ///tapi hal tsb hanya memberitahu kita bawah sintak tsb valid
 }
