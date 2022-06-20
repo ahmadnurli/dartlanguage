7acac1d6408085241cc88ead209da161c87d4182
ahmad <ahmad.nurli@moratelindo.co.id>
2022-06-20 09:30:18 +0700

    The const keyword

--- bin/dart_type_system.dart
+++ bin/dart_type_system.dart
@@ -1,61 +1,51 @@
 void main(List<String> args) {
-  ///Ada akalanya waktu di mana kita ingin mendeklarasikan dan inisialisasi sebuah variable
-  ///yang tidak akan mengubah nilai setelahnya, dgn kata lain kita ingin varible kita hanya bisa di baca saja
-  ///oleh karena itu kita bisa menggunakan 'final' keyward seperti dibawah ini:
-  ///dgn begitu kita memberitahu Dart,
-  ///mendeklarasikan dan menginisialisasi final variable untuk kita, dan jangan mengubahnya lagi
-  final name = 'ahmad';
-  final age = 22;
-  final height = 65.6;
-
-  ///ketika kita coba untuk mengetik sprti dibawah ini:
-  ///dart akan memberitahu bahwa final variable age hanya bisa di set satu kali
-  // age = 21;
-
-  ///lalu kenapa kita melakukan hal diatas
-  ///jika menggunakan var, kita bisa menerapkan kembali dgn nilai berbeda
-  ///dgn pengertian tsb, penggunaan final menjadi lebih terbatas
+  ///pada pelajaran terakhir, kita telah belajar pendefinisian variable
+  ///yang hanya bisa dibaca oleh program dgn 'final' keyword, dan lebih cenderung menggunakan
+  ///'final' dari pada 'var' jika memungkinkan
   ///
-  ///sebenarnya ini sesuatu yang baik, karena jika kita membatasi jumlah kemungkinan state
-  ///yang dapat dimasuki oleh program kita, kemungkinan kecilnya akan menimbulkan bug
+  ///dalam Dart ada satu keyword yg mempunyai pembatasan lebih
+  ///dari pada final yang disebut 'const'
   ///
-  ///sebagai contoh program sederhana pada sebuah string yg akan diubah menjadi huruf besar
-  ///dan jika kita mendeklarasikannya dengan var,
-  ///tidak terjadi hal apapun dalam menerapkan method uppercase dibawah ini
-  var title = 'Dart course';
-  var titleUppercased = title.toUpperCase();
+  ///jika kita buat pertimbangan dari kode yg sdh pernah kita tulis
+  ///kita bisa mendeklarasikan variable sbg const
+  ///dgn begini kita mendifinisikan sebuah compile-time constant
+  ///'variable 'const' memiliki performa yg sangat baik, jika kita menggunakan const
+  ///dart bisa mengoptimasisakan kode yang dihasilkan dan program yg lebih efisien
+  const title = 'Dart Course';
+
+  ///sehingga dalam kasus ini kita mendeklarasikan variable sbg const,
+  ///karena disini menginisialisikan string literal,
+  ///dan semua literal menngkompilasi(compile time) dirinya sendiri
+  ///
+  ///jika kita coba mendeklarasikan variable sbg const
+  ///ini akan terjadi error yang memberitahu kita const variable harus diinisialisasi
+  ///dgn nilai konstant,
+  ///dan karenanya Dart hanya bisa mengevaluasi nilai dari expression ini ketika runtime
+  ///sehingga kita tidak bisa menerapkan(assign) nya ke variable const
+  const titleUppercased = title.toUpperCase();
+
+  ///disisi lain ada operasi tertantu yang mana Dart bisa memperbaikinya ketika compile time.
   print(titleUppercased);
 
-  ///tetapi jika kita bertujuan menginisialisasi titel dgn nilai baru dan
-  ///mengkonversi menjadi huruf besar, seharusnya tidak kita lakukan demikian
-  var titel = 'Dart course';
-  titel = '24';
+  ///sbg contoh kita mendefinisikan dua buah variable const:
+  const x = 1;
+  const y = 2;
 
-  ///dan karena alasan ini, lebih baik mendeklarasikan variable tsb sbg final
-  ///sehingga dart akan memberitahu bahwa kita tidak merubah final variable
-  final titles = 'Dart course';
-  // titles = '25';
+  ///dan kita mendifinisikan const sprt ini juga:
+  ///ini merupakan sintak yg valid, karena Dart cukup pintar untuk mengevaluasi x + y sbg
+  ///compile time constant, sehingga z juga bisa menggunakan const.
+  ///karena semua variable ini const, maka hanya bisa di set satu kali, sprt variable 'final'
+  const z = x + y;
+  print(z);
 
-  ///begitu juga setelah kita memperkirakan hasil keluaran dari variable tidak berubah,
-  ///maka kita harus mendeklarasikannya sbg final juga
-  final titlesUppercased = titles.toUpperCase();
-  print(titlesUppercased);
+  ///dalam artian kita tidak bisa menulis kode sprt di bawah ini:
+  z = 4; // dart akan memberitahu bahwa sintak ini tidak valid.
 
-  ///tentu sj ini hanya program yang simple,
-  ///dan mungkin kita tidak yakin bahwa ini ada masalah besar
-  ///tapi saat kita mempunya program yang lebih besar dgn banyak kode
-  ///maka menggunakan final akan benar2 sangat terbantu,
-  ///dan juga memberitahu kita variable mana sj yg ditetapkan hanya bisa untuk dibaca
+  ///sbg pengingat, bagaimana cara kerja dari 'var', 'final', dan 'const'
+  ///kita bisa menggunakan dan menge-set 'var' lebih dari satu kali
+  ///kita bisa menggunakan dan menge-set 'final' hanya satu kali(variable yg hanya bisa dibaca sj)
+  ///dan kita bisa menggunakna dan meng-set 'const' untuk variable2 yg merupakan compile time constants.
   ///
-  ///jadi, kesimpulan dari pelajaran ini adalah bahwa final berarti hanya bisa dibaca,
-  ///dan kita harus memilih/menetapkan final atau var bila memungkinkan
-  ///
-  ///btw, kita kita menggunakan final, kita masih bisa diizinkan untuk secara eksplisit
-  ///mendeklarasikan jenis variable kita
-  ///dalam artian kita bisa menulis kode kita sprt berikut:
-  ///bagaiamanapun jg ini tidak diperlukan, karena Dart akan menyimpulkan(infer)
-  ///tipe variable untuk kita. jadi kita tidak membutuhkannya
-  final String judul = 'Dart course';
-
-  ///tapi hal tsb hanya memberitahu kita bawah sintak tsb valid
+  ///dan dalam sebuah peraturan yg praktis, kita harus selalu memilih 'const' ketimbang 'final'
+  ///dan kita harus selalu memilih 'final' ketimbang 'var'
 }
