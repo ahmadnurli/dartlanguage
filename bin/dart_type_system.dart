fe5d94993fca9e659e498d1e81f2d3c5359b9fb7
ahmad <ahmad.nurli@moratelindo.co.id>
2022-06-24 08:17:06 +0700

    if/else statements

--- bin/dart_type_system.dart
+++ bin/dart_type_system.dart
@@ -1,10 +1,18 @@
 void main(List<String> args) {
-  ///Control flow
+  ///sbg contoh:
   ///
-  ///if / else statements
-  ///while and for loops
-  ///switch statements
-  ///enumerations
-  ///
-  ///special key : break & continue on loops and switch statements
+  ///karena semua logic berdasarakan umur, maka umur kita definisikan sgb const
+  ///penginisialisasi ini dgn compile-time constant
+  const age = 14;
+  if (age < 16) {
+    print('Juniar ticket');
+    print('Prices is \$8');
+  } else if (age >= 60) {
+    print('Senior ticket');
+    print('Prices is \$6');
+  } else {
+    print('Regular ticket');
+    print('Prices is \$10');
+  }
+  print('Enjoy your visit');
 }
